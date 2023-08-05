3a30fb6ac6072e68380e502343f4ea5d28639584
Ankit <ankitsrivastava7835@gmail.com>
2023-08-05 14:47:36 +0530

    chnages

--- mail/src/main/java/mailService/mail/MailApplication.java
+++ mail/src/main/java/mailService/mail/MailApplication.java
@@ -2,12 +2,14 @@ package mailService.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
 
 }
--- mail/src/main/java/mailService/mail/service/EmailService.java
+++ mail/src/main/java/mailService/mail/service/EmailService.java
@@ -1,6 +1,5 @@
 package mailService.mail.service;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import freemarker.template.Configuration;
@@ -13,12 +12,11 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import static mailService.mail.constant.MailConstant.ACCOUNT_CREATE_DEFAULT_SUBJECT_EMAIL;
-
 @Service
 public class EmailService {
     JavaMailSenderService javaMailSenderService;
     ProcessTemplate processTemplate;
-    Logger logger = LoggerFactory.getLogger("");
+    Logger log = LoggerFactory.getLogger("");
 
     public EmailService(JavaMailSenderService javaMailSenderService) {
         this.javaMailSenderService = javaMailSenderService;
@@ -30,6 +28,8 @@ public class EmailService {
 
         Map<String, Object> requestData = JSONToObject.convertToObject(msg, Map.class);
 
+        log.info("Email Payload " + requestData);
+
         Mail mail = new Mail();
         mail.setToEmail(requestData.get("email").toString());
         mail.setSubject(ACCOUNT_CREATE_DEFAULT_SUBJECT_EMAIL);
@@ -37,8 +37,9 @@ public class EmailService {
         String template = processTemplate.setDataInTemplate("account_create.ftl", requestData);
         mail.setContent(template);
         javaMailSenderService.sendMail(mail);
-        logger.info("Response {}", requestData);
+        log.info("Response {}", requestData);
         System.out.println("Kafka Subscriber method called " + msg);
+        throw new NullPointerException("Null value");
     }
 
     @KafkaListener(topics = "login_verification", groupId = "sendEmail")
@@ -54,7 +55,7 @@ public class EmailService {
         String template = processTemplate.setDataInTemplate("login_verification_template.ftl", data);
         mail.setContent(template);
         javaMailSenderService.sendMail(mail);
-        logger.info("Response {}", requestData);
+        log.info("Response {}", requestData);
 
     }
 }
